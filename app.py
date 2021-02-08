1f096b596348d113495a8e19e7977b8428e256a4
insincerity <kdglinkina@edu.hse.ru>
2021-02-08 23:08:41 +0300

    Achivement2

---
+++ app.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+Message = namedtuple('Message', 'number')
+messages = []
+
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('main.html', messages=messages)
+
+
+@app.route('/add_number', methods=['POST'])
+def add_message():
+    x = request.form['calc']
+    messages.append(Message(x))
+    return redirect(url_for('main'))
+
+
+if __name__ == '__main__':
+    app.run()
